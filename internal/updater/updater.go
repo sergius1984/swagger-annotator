b3bbfc1b5eaa7f350f5d836118259a2fa4ee458e
Sergius <sergius123456789@ya.ru>
2026-07-02 17:13:14 +0300

    GENERATED WORK

--- internal/updater/updater.go
+++ internal/updater/updater.go
@@ -1,12 +1,8 @@
 package updater
 
 import (
-	"bytes"
 	"fmt"
 	"go/ast"
-	"go/format"
-	"go/parser"
-	"go/token"
 	"os"
 	"path/filepath"
 	"strings"
@@ -15,55 +11,92 @@ import (
 	"swagger-annotator/internal/config"
 )
 
-// UpdateComments вставляет/обновляет Swagger-комментарий для указанной функции.
-// dryRun – если true, то файл не перезаписывается.
+// UpdateComments вставляет / обновляет Swagger-комментарий для указанной функции.
 func UpdateComments(fileName, funcName, recvType string, def *config.HandlerDef, dryRun bool, projectDir string) error {
 	if dryRun {
 		return nil
 	}
 	fullPath := filepath.Join(projectDir, filepath.FromSlash(fileName))
-	fset := token.NewFileSet()
-	file, err := parser.ParseFile(fset, fullPath, nil, parser.ParseComments)
+
+	content, err := os.ReadFile(fullPath)
 	if err != nil {
-		return fmt.Errorf("парсинг %s: %w", fileName, err)
+		return fmt.Errorf("чтение %s: %w", fileName, err)
 	}
 
-	var target *ast.FuncDecl
-	for _, decl := range file.Decls {
-		fn, ok := decl.(*ast.FuncDecl)
-		if !ok {
-			continue
-		}
-		if fn.Name.Name == funcName {
-			if recvType != "" {
-				if fn.Recv == nil || len(fn.Recv.List) == 0 {
-					continue
-				}
-				rType := typeToString(fn.Recv.List[0].Type)
-				rType = strings.TrimPrefix(rType, "*")
-				if rType != recvType {
-					continue
-				}
+	lines := strings.Split(string(content), "\n")
+	insertLine := -1
+
+	// Ищем строку с объявлением нужной функции
+	for i, line := range lines {
+		trimmed := strings.TrimSpace(line)
+		if recvType == "" {
+			// обычная функция
+			if strings.HasPrefix(trimmed, "func "+funcName+"(") ||
+				strings.HasPrefix(trimmed, "func "+funcName+" ") {
+				insertLine = i
+				break
 			}
-			target = fn
-			break
+		} else {
+			// метод
+			if strings.Contains(trimmed, "func (") &&
+				strings.Contains(trimmed, ") "+funcName+"(") &&
+				strings.Contains(trimmed, recvType) {
+				insertLine = i
+				break
+			}
 		}
 	}
-	if target == nil {
-		return fmt.Errorf("функция %s не найдена", funcName)
+
+	if insertLine == -1 {
+		return fmt.Errorf("функция %s не найдена в файле", funcName)
 	}
 
 	newDoc := annotations.GenerateSwaggerDoc(def)
-	target.Doc = newDoc
-
-	var buf bytes.Buffer
-	if err := format.Node(&buf, fset, file); err != nil {
-		return fmt.Errorf("форматирование: %w", err)
+	var commentLines []string
+	for _, c := range newDoc.List {
+		commentLines = append(commentLines, c.Text)
 	}
-	return os.WriteFile(fullPath, buf.Bytes(), 0644)
+
+	funcLine := lines[insertLine]
+	indent := ""
+	if len(funcLine) > 0 {
+		for _, ch := range funcLine {
+			if ch == ' ' || ch == '\t' {
+				indent += string(ch)
+			} else {
+				break
+			}
+		}
+	}
+
+	var newCommentLines []string
+	for _, cl := range commentLines {
+		newCommentLines = append(newCommentLines, indent+cl)
+	}
+
+	// Удаляем предыдущий doc-комментарий (если был)
+	removeStart := insertLine
+	for i := insertLine - 1; i >= 0; i-- {
+		trimmed := strings.TrimSpace(lines[i])
+		if strings.HasPrefix(trimmed, "//") || trimmed == "" {
+			removeStart = i
+		} else {
+			break
+		}
+	}
+
+	newLines := make([]string, 0, len(lines))
+	newLines = append(newLines, lines[:removeStart]...)
+	newLines = append(newLines, newCommentLines...)
+	newLines = append(newLines, lines[insertLine:]...)
+
+	newContent := strings.Join(newLines, "\n")
+	if err := os.WriteFile(fullPath, []byte(newContent), 0644); err != nil {
+		return fmt.Errorf("запись %s: %w", fileName, err)
+	}
+	return nil
 }
 
-// Вспомогательная функция (можно вынести в общий util, но для простоты здесь)
 func typeToString(expr ast.Expr) string {
 	switch t := expr.(type) {
 	case *ast.Ident:
